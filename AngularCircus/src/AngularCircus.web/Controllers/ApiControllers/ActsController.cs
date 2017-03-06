ce01be680458058b359173c0d8e019fb162e03bd
ptank <kptankersley@gmail.com>
2017-03-06 10:53:25 -0500

    Changed controllers oto plural and fixed act add button

---
+++ AngularCircus/src/AngularCircus.web/Controllers/ApiControllers/ActsController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AngularCircus.web.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using AngularCircus.web.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using AngularCircus.web.Controllers.ApiControllers;
+
+
+namespace AngularCircus.web.Controllers.ApiControllers
+{
+    [Produces("application/json")]
+    // [Route("~/act")]
+    public class ActsController : Controller
+    {
+        private readonly AngularCircusContext _context;
+
+        private UserManager<ApplicationUser> _userManager { get; set; }
+        
+
+        public ActsController(UserManager<ApplicationUser> userManager, AngularCircusContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+
+
+        [Route("~/circuses/{id}/acts")]
+        public IActionResult Act(int id)
+        {
+            var circus = _context.Circuses.Include(q => q.Acts).FirstOrDefault(m => m.Id == id);
+            return View(circus);
+        }
+
+        [HttpGet("~/api/circuses/{circusId}/acts")]
+        public IEnumerable<Act> GetActs()
+        {
+            var userId = _userManager.GetUserId(User);
+            
+
+            return _context.Acts
+                .Where(q => q.Circus.Owner == userId).ToList();
+        }
+
+        // GET api/acts/5
+        [HttpGet("api/circuses/{circusId}/acts/{id}")]
+        public async Task<IActionResult> GetAct(int circusId) //, int id) //he only had one
+        {
+            
+
+            var userId = _userManager.GetUserId(User);
+
+            var circus = _context.Circuses.Include(q => q.Acts).FirstOrDefault(q => q.Id == circusId);
+            var act = circus.Acts.FirstOrDefault(q => q.Id == circusId); //== id); //not sure if need circusId or id. He had one and hten the other soemhwere
+
+            if (act == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(act);
+        }
+
+
+
+        // POST api/acts
+        [HttpPost("~/api/circuses/{circusId}/acts")]
+        public async Task<IActionResult> PostAct(int id, [FromBody]Act act)
+        {
+            var circus = _context.Circuses.FirstOrDefault(q => q.Id == id);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            act.Owner =  _userManager.GetUserId(User);
+
+            circus.Acts.Add(act);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                if (ActExists(act.Id))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            //var fixActs = _context.Database.ExecuteSqlCommand("update Acts set Acts.CircusId = C.Id from Acts as A inner join Circuses as C on C.Owner = A.Owner");
+            return CreatedAtAction("GetAct", new { id = act.Id }, act);
+
+        }
+
+        // PUT api/acts/5
+        [HttpPut("~/api/circuses/{circusId}/acts/{id}")]
+        public async Task<IActionResult> PutAct(int id, [FromBody] Act act)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != act.Id)
+            {
+                return BadRequest();
+            }
+
+            act.Owner = _userManager.GetUserId(User);
+            _context.Entry(act).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+
+        // DELETE api/circuses/5
+        [HttpDelete("~/api/circuses/{circusId}/acts/{id}")]
+        public async Task<IActionResult> DeleteAct(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userId = _userManager.GetUserId(User);
+
+            Act act = await _context.Acts
+                .Where(q => q.Circus.Owner == userId)
+                .SingleOrDefaultAsync(m => m.Circus.Id == m.Id);
+
+            if (act == null)
+            {
+                return NotFound();
+            }
+
+            _context.Acts.Remove(act);
+            await _context.SaveChangesAsync();
+
+            return Ok(act);
+
+        }
+
+        private bool ActExists(int id)
+        {
+            var userId = _userManager.GetUserId(User);
+            return _context.Acts.Any(e => e.Owner == userId && e.Id == id);
+        }
+
+
+
+
+    }
+}
---
+++ AngularCircus/src/AngularCircus.web/Controllers/ApiControllers/CircusesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AngularCircus.web.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using AngularCircus.web.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
+
+
+namespace AngularCircus.web.Controllers.ApiControllers
+{
+    [Produces("application/json")]
+    //[Route("~/circus")]
+    [Authorize]
+    public class CircusesController : Controller
+    {
+        private readonly AngularCircusContext _context;
+
+        private UserManager<ApplicationUser> _userManager { get; set; }
+        
+
+        public CircusesController(UserManager<ApplicationUser> userManager, AngularCircusContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+
+        }
+
+        [Route("~/circuses/")]
+        [Authorize(ActiveAuthenticationSchemes = "Identity.Application")]
+        public IActionResult Circus()
+        {
+            return View();
+        }
+
+        [HttpGet("~/api/circuses")]
+        public IEnumerable<Circus> GetCircuses()
+        {
+            var userId = _userManager.GetUserId(User);
+            return _context.Circuses.Where(q => q.Owner == userId).ToList();
+        }
+
+        // GET api/circuses/5
+        [HttpGet("~/api/circuses/{id}")]
+        public async Task<IActionResult> GetCircus(int id)
+        {
+
+            var userId = _userManager.GetUserId(User);
+            Circus circus = await _context.Circuses
+                .SingleOrDefaultAsync(m => m.Owner == userId && m.Id == id);
+
+            if (circus == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(circus);
+        }
+
+
+
+        // POST api/circuses
+        [HttpPost("~/api/circuses")]
+        public async Task<IActionResult> PostCircus([FromBody]Circus circus)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            circus.Owner = _userManager.GetUserId(User);
+            _context.Circuses.Add(circus);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                if (CircusExists(circus.Id))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return CreatedAtAction("GetCircus", new { id = circus.Id }, circus);
+        }
+
+        // PUT api/circuses/5
+        [HttpPut("~/api/circuses/{id}")]
+        public async Task<IActionResult> PutCircus(int id, [FromBody] Circus circus)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != circus.Id)
+            {
+                return BadRequest();
+            }
+
+            circus.Owner = _userManager.GetUserId(User);
+            _context.Entry(circus).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CircusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+
+        // DELETE api/circuses/5
+        [HttpDelete("~/api/circuses/{id}")]
+        public async Task<IActionResult> DeleteCircus(int id)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userId = _userManager.GetUserId(User);
+
+            Circus circus = await _context.Circuses
+                .Where(q => q.Owner == userId)
+                .SingleOrDefaultAsync(m => m.Id == id);
+
+            if(circus == null)
+            {
+                return NotFound();
+            }
+
+            _context.Circuses.Remove(circus);
+            await _context.SaveChangesAsync();
+
+            return Ok(circus);
+        }
+        
+        private bool CircusExists(int id)
+        {
+            var userId = _userManager.GetUserId(User);
+            return _context.Circuses.Any(e => e.Owner == userId && e.Id == id);
+        }
+    }
+}
