e587d025d69f9bd379915fac6cbdf845025ca62a
Mackenzie Lewerke <mackenzielewerke@gmail.com>
2017-03-08 14:57:30 -0500

    More cleanup.

--- AngularCircus/src/AngularCircus.web/wwwroot/_references.js
+++ AngularCircus/src/AngularCircus.web/wwwroot/_references.js
@@ -1,16 +1,12 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="js/application.js" />
-<<<<<<< HEAD
-/// <reference path="js/controllers/ActController.js" />
-/// <reference path="js/controllers/authentication.js" />
-/// <reference path="js/controllers/CircusController.js" />
-/// <reference path="js/controllers/PerformerController.js" />
-=======
-/// <reference path="js/controllers/animalscontroller.js" />
-/// <reference path="js/controllers/authentication.js" />
-/// <reference path="js/controllers/exhibitscontroller.js" />
-/// <reference path="js/controllers/zooscontroller.js" />
->>>>>>> c759d79937d0aba725697a999695226d961a5a0c
+/// <reference path="js/controllers/actscontroller.js" />
+/// <reference path="js/controllers/AnimalsController.js" />
+/// <reference path="js/controllers/Authentication.js" />
+/// <reference path="js/controllers/circusescontroller.js" />
+/// <reference path="js/controllers/ExhibitsController.js" />
+/// <reference path="js/controllers/performerscontroller.js" />
+/// <reference path="js/controllers/ZoosController.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
